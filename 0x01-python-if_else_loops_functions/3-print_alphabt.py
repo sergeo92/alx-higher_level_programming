01e1ab3d651034243830c0fa0978e209dfe47a81
Arobase <sergedelonboka92@gmail.com>
2022-11-29 15:46:39 -0800

    Print lower alphabet except e and q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_lower_alphabet():
+    for letter in "abcdfghijklmnoprstuvwxyz":
+        print('{}'.format(letter), end="")
+
+
+def main():
+    print_lower_alphabet()
+
+
+if __name__ == "__main__":
+    main()
