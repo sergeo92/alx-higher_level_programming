f6681b68f70af1bca2799c915405f08f0763cf85
Arobase <sergedelonboka92@gmail.com>
2022-12-01 16:45:09 -0800

    Program that import function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import add_0
+
+
+def main():
+    a = 1
+    b = 2
+    sum_n = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, sum_n))
+
+
+if __name__== "__main__":
+    main()
