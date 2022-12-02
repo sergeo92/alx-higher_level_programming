ff9889cf444a5c2a2e98a5c1c0313d25f4fb4aa2
Arobase <sergedelonboka92@gmail.com>
2022-12-01 17:12:39 -0800

    Programm that import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -8,7 +8,7 @@ def main():
     a = 1
     b = 2
     sum_n = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, sum_n))
+    print("{:d} + {:d} = {:d}".format(a, b, sum_n))
 
 
 if __name__== "__main__":
