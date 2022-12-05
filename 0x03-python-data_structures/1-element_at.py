7256c400f1236e89637ce3c065419a5717555217
Arobase <sergedelonboka92@gmail.com>
2022-12-04 17:01:57 -0800

    function that retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -11,7 +11,7 @@ def element_at(my_list, idx):
     """
 
     index_last = len(my_list)-1
-    if idx < 0 or idx > index_last:
+    if idx < 0 or idx >= index_last:
         return None
     else:
         return  my_list[idx]
