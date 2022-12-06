19c5d64e55435ddff43711b531302b873fab5cce
Arobase <sergedelonboka92@gmail.com>
2022-12-05 19:21:04 -0800

    function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    
+    if lenght == 0:
+        return lenght, None
+
+    return lenght, sentence[0]
+
+
+if __name__ == "__main__":
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
+
