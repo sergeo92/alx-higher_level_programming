dab5739257f658589aec329d926728b0db0c9682
Arobase <sergedelonboka92@gmail.com>
2022-12-22 09:29:44 -0800

    function that computes a to the power of b and return the value.

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / (a**(-b))
+    else:
+        return a**b
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
