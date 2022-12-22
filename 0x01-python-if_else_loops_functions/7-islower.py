246a4a84b06676b54997bcb849867d81ba68907d
Arobase <sergedelonboka92@gmail.com>
2022-12-22 15:14:45 -0800

    function that checks for lowercase character.

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if c in "abcdefghijklmnopqrstuvwxyz":
+        return True
+    else:
+        return False
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
