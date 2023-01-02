12a1618de3ba623c0fc1178eda4d20369caaddda
Arobase <sergedelonboka92@gmail.com>
2023-01-02 01:47:07 -0800

    unction that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+
+if __name__ == "__main__":
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
