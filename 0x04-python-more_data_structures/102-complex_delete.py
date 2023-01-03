138ac8a28bf9b8926bb82f1dd5c6efcc65ed40d0
Arobase <sergedelonboka92@gmail.com>
2023-01-03 00:17:01 -0800

    function that deletes keys with a specific value in a dictionary.

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
+
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary:
-        return a_dictionary
-
-    for key in a_dictionary:
-        if key == value:
-            del a_dictionary[value]
-
+    new_dict = dict(a_dictionary)
+    for k, v in new_dict.items():
+        if v == value:
+            a_dictionary.pop(k)
     return a_dictionary
-
-
-if __name__ == "__main__":
-    pass
