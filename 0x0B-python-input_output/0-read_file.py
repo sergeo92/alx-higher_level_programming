cac07d9e2ed785a783c349c342fa0095618d5d1f
Arobase <sergedelonboka92@gmail.com>
2023-01-10 04:15:55 -0800

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    with open(filename, encoding="UTF8") as my_file:
+        for line in my_line:
+            line = line.replace("\n", "")
+            print(line)
