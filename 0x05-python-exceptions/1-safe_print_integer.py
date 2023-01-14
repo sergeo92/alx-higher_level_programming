7c2c47b2032f13184855b3b9b05302c9eb948fd1
Arobase <sergedelonboka92@gmail.com>
2023-01-14 04:58:00 -0800

    function that prints an integer with

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,18 @@ def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
         return True
-    except ValueError, TypeError:
+    except ValueError:
         return False
+    except TypeError:
+        return False
+
+if __name__ == '__main__':
+    value = 89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
